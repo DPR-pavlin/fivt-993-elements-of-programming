2db6401285a8a9d92335d6497a09e1e2bfb37dfa
Daniil Gusev <gusdzerzhi@yandex.ru>
2012-02-02 12:15:25 +0400

    add persistent_heap.h

---
+++ stlext/persistent_heap.h
@@ -0,0 +1,18 @@
+#ifndef PERSISTENT_HEAP_H_
+#define PERSISTENT_HEAP_H_
+
+template<class T, class Comparator>
+class persistent_heap{
+
+ public:
+
+
+
+ private:
+
+
+};
+
+
+
+#endif /* PERSISTENT_HEAP_H_ */
